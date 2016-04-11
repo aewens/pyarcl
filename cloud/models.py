de07329a141633e22068dd1ff8f15fc40d73efb8
aewens <email@aewens.com>
2016-04-10 23:50:56 -0500

    Migrations for cloud

--- cloud/models.py
+++ cloud/models.py
@@ -1,5 +1,13 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 # Create your models here.
+class ItemList(models.Model):
+    name = models.CharField(max_length=256)
+    group = models.CharField(max_length=128)
+    link = models.CharField(max_length=1024)
+
+class Item(models.Model):
+    item_list = models.ForeignKey(ItemList, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=16)
+    file_name = models.CharField(max_length=256)
